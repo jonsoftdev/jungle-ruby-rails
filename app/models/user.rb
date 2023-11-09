80d478d3152edd174cd70e1a8f9bff1a43baf51c
JonSoft Solutions <jonathandev25@gmail.com>
2023-11-08 22:22:47 -0500

    add presence validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :password, :on => :create
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
