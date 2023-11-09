a33c82ba1f112ea2fe32e0a7c86eb7c980f7ea85
JonSoft Solutions <jonathandev25@gmail.com>
2023-11-08 19:39:00 -0500

    implement test validation for product

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -10,4 +10,28 @@ RSpec.describe Product, type: :model do
     expect(@product).to be_valid
   end
   
+  it "is not valid without a name" do
+    @product.name = nil
+    expect(@product).to_not be_valid
+    expect(@product.errors.full_messages).to include("Name can't be blank")
+  end
+
+  it "is not valid without a price" do
+    @product.price_cents = nil
+    expect(@product).to_not be_valid
+    expect(@product.errors.full_messages).to include("Price can't be blank")
+  end
+
+  it "is not valid without a quantity" do
+    @product.quantity = nil
+    expect(@product).to_not be_valid
+    expect(@product.errors.full_messages).to include("Quantity can't be blank")
+  end
+
+
+  it "is not valid without a category" do
+    @product.category = nil
+    expect(@product).to_not be_valid
+    expect(@product.errors.full_messages).to include("Category can't be blank")
+  end
 end
