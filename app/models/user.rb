45fcc607165dfd27fd6fc4b454ef4f6b5a232ffc
JonSoft Solutions <jonathandev25@gmail.com>
2023-11-10 23:45:00 -0500

    Add authenticate with credentials test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: { case_sensitive: false }, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 end
